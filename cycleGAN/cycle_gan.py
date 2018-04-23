d38ba75e1c062c7c9ed7e4a6aee14af7271409c8
chengbin <sbkim0407@gmail.com>
2018-04-24 01:04:36 +0900

    revise error in consistent loss

--- cycleGAN/cycle_gan.py
+++ cycleGAN/cycle_gan.py
@@ -26,7 +26,7 @@ class cycleGAN(object):
         # [instance|batch] use instance norm or batch norm, default: instance
         self.norm = 'instane'
         self.lambda1, self.lambda2 = 10.0, 10.0
-        self.ngf, self.ndf = 32, 64
+        self.ngf, self.ndf = 64, 64
         self.real_label = 0.9
         self.start_dcay_step = 100000
         self.decay_steps = 100000
@@ -81,7 +81,9 @@ class cycleGAN(object):
         Dy_optim = self.optimizer(loss=self.Dy_dis_loss, variables=self.Dy_dis.variables, name='Adam_Dy')
         F_optim = self.optimizer(loss=self.F_loss, variables=self.F_gen.variables, name='Adam_F')
         Dx_optim = self.optimizer(loss=self.Dx_dis_loss, variables=self.Dx_dis.variables, name='Adam_Dx')
-        self.optims = tf.group([G_optim, Dy_optim, F_optim, Dx_optim])
+        # self.optims = tf.group([G_optim, Dy_optim, F_optim, Dx_optim])
+        with tf.control_dependencies([G_optim, Dy_optim, F_optim, Dx_optim]):
+            self.optims = tf.no_op(name='optimizers')
 
         # for sampling function
         self.fake_y_sample = self.G_gen(self.x_test_tfph)
@@ -108,7 +110,7 @@ class cycleGAN(object):
 
     def cycle_consistency_loss(self, x_imgs, y_imgs):
         forward_loss = tf.reduce_mean(tf.abs(self.F_gen(self.G_gen(x_imgs)) - x_imgs))
-        backward_loss = tf.reduce_mean(tf.abs(self.G_gen(self.F_gen(y_imgs) - y_imgs)))
+        backward_loss = tf.reduce_mean(tf.abs(self.G_gen(self.F_gen(y_imgs)) - y_imgs))
         loss = self.lambda1 * forward_loss + self.lambda2 * backward_loss
         return loss
 
@@ -215,7 +217,7 @@ class cycleGAN(object):
 
 
 class Generator(object):
-    def __init__(self, name=None, ngf=32, norm='instance', image_size=(128, 256, 3), _ops=None):
+    def __init__(self, name=None, ngf=64, norm='instance', image_size=(128, 256, 3), _ops=None):
         self.name = name
         self.ngf = ngf
         self.norm = norm
@@ -227,26 +229,26 @@ class Generator(object):
         with tf.variable_scope(self.name, reuse=self.reuse):
             tf_utils.print_activations(x)
 
-            # (N, H, W, C) -> (N, H, W, 32)
+            # (N, H, W, C) -> (N, H, W, 64)
             conv1 = tf_utils.padding2d(x, p_h=3, p_w=3, pad_type='REFLECT', name='conv1_padding')
             conv1 = tf_utils.conv2d(conv1, self.ngf, k_h=7, k_w=7, d_h=1, d_w=1, padding='VALID',
                                     name='conv1_conv')
             conv1 = tf_utils.norm(conv1, _type='instance', _ops=self._ops, name='conv1_norm')
             conv1 = tf_utils.relu(conv1, name='conv1_relu', is_print=True)
 
-            # (N, H, W, 32)  -> (N, H/2, W/2, 64)
+            # (N, H, W, 64)  -> (N, H/2, W/2, 128)
             conv2 = tf_utils.conv2d(conv1, 2*self.ngf, k_h=3, k_w=3, d_h=2, d_w=2, padding='SAME',
                                     name='conv2_conv')
             conv2 = tf_utils.norm(conv2, _type='instance', _ops=self._ops, name='conv2_norm',)
             conv2 = tf_utils.relu(conv2, name='conv2_relu', is_print=True)
 
-            # (N, H/2, W/2, 64) -> (N, H/4, W/4, 128)
+            # (N, H/2, W/2, 128) -> (N, H/4, W/4, 256)
             conv3 = tf_utils.conv2d(conv2, 4*self.ngf, k_h=3, k_w=3, d_h=2, d_w=2, padding='SAME',
                                     name='conv3_conv')
             conv3 = tf_utils.norm(conv3, _type='instance', _ops=self._ops, name='conv3_norm',)
             conv3 = tf_utils.relu(conv3, name='conv3_relu', is_print=True)
 
-            # (N, H/4, W/4, 128) -> (N, H/4, W/4, 128)
+            # (N, H/4, W/4, 256) -> (N, H/4, W/4, 256)
             if (self.image_size[0] <= 128) and (self.image_size[1] <= 128):
                 # use 6 residual blocks for 128x128 images
                 res_out = tf_utils.n_res_blocks(conv3, num_blocks=6, is_print=True)
@@ -254,17 +256,17 @@ class Generator(object):
                 # use 9 blocks for higher resolution
                 res_out = tf_utils.n_res_blocks(conv3, num_blocks=9, is_print=True)
 
-            # (N, H/4, W/4, 128) -> (N, H/2, W/2, 64)
+            # (N, H/4, W/4, 256) -> (N, H/2, W/2, 128)
             conv4 = tf_utils.deconv2d(res_out, 2*self.ngf, name='conv4_deconv2d')
             conv4 = tf_utils.norm(conv4, _type='instance', _ops=self._ops, name='conv4_norm')
             conv4 = tf_utils.relu(conv4, name='conv4_relu', is_print=True)
 
-            # (N, H/2, W/2, 64) -> (N, H, W, 32)
+            # (N, H/2, W/2, 128) -> (N, H, W, 64)
             conv5 = tf_utils.deconv2d(conv4, self.ngf, name='conv5_deconv2d')
             conv5 = tf_utils.norm(conv5, _type='instance', _ops=self._ops, name='conv5_norm')
             conv5 = tf_utils.relu(conv5, name='conv5_relu', is_print=True)
 
-            # (N, H, W, 32) -> (N, H, W, 3)
+            # (N, H, W, 64) -> (N, H, W, 3)
             conv6 = tf_utils.padding2d(conv5, p_h=3, p_w=3, pad_type='REFLECT', name='output_padding')
             conv6 = tf_utils.conv2d(conv6, self.image_size[2], k_h=7, k_w=7, d_h=1, d_w=1,
                                     padding='VALID', name='output_conv')
